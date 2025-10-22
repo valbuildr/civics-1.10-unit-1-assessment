7817f7874fbfd066ea21e6f334c6e329a99369fd
valbuildr <150450259+valbuildr@users.noreply.github.com>
2025-10-22 11:57:50 -0500

    Deploying to Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://astronaut.github.io',
+  base: '/civics-1.10-unit-1-assessment',
 });
